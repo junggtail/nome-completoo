f9fa510dc0d449e7e150d961b1577e968309d6c5
junggtail <e107ti@etec.sp.gov.br>
2023-09-25 19:56:59 -0300

    atividade nome completo

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿string nome, sobrenome;
+Console.Write("Fernanda.......: ");
+nome = Console.ReadLine()!;
+
+Console.Write("Oliveira.: ");
+sobrenome = Console.ReadLine()!;
+
+Console.WriteLine($"Fernanda Oliveira: {nome} {sobrenome}");
+Console.WriteLine($"Oliveira Fernanda: {sobrenome.ToUpper()}, {nome}");
